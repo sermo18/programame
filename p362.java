68b60fe1e8e652062f891f785baf5e2a847fc981
Patxi SL DAW19 <patxisldaw19@gmail.com>
2019-10-02 23:36:43 +0200

    programas de prueba

---
+++ p362.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class p362 {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int ncasos = sc.nextInt();
+		int dia;
+		int mes;
+
+		for(int i=0; i<ncasos; i++){
+			
+			
+		}
+
+	}
+}
