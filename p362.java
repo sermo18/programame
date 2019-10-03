30af722a4a81bc27504f10244955fbb9c2abe5ad
Khoyac <Khoyac@gmail.com>
2019-10-03 20:32:48 +0200

    Ejercicio 362 Resuelto

--- p362.java
+++ p362.java
@@ -10,7 +10,13 @@ public class p362 {
 		int mes;
 
 		for(int i=0; i<ncasos; i++){
-			
+			dia=sc.nextInt();
+			mes=sc.nextInt();
+			if(dia==25 && mes==12) {
+				System.out.println("SI");
+			} else {
+				System.out.println("NO");
+			}			
 			
 		}
 
