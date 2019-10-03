c545f086e7388643b04a6f163612377309e89f42
Patxi Sanchis <patxisldaw19@gmail.com>
2019-10-03 19:11:40 +0200

    programa 131 añadido

---
+++ p131.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class p131 {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int lmio,lvecino,bmio,bvecino,pmio,pvecino;
+        while (sc.hasNextInt()) {
+            lmio = sc.nextInt();
+            lvecino = sc.nextInt();
+            bmio = sc.nextInt();
+            bvecino = sc.nextInt();
+            pmio = sc.nextInt();
+            pvecino = sc.nextInt();
+            
+            int vmio = 0;
+            int vvecino = 0;
+            
+            if ( (lmio != 0 && lvecino != 0) ){
+            
+                if (lmio<=bmio) vmio += 1;
+                if (lvecino<=bvecino) vvecino = 1;
+                
+                
+            
+            
+            
+            
+            }
+                
+        
+        
+		
+			
+			
+		}
+
+	}
+}
