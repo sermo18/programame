a2bd460375bd514a696e8bbf466fa89b20bbcded
Patxi SL DAW19 <55405142+patxisldaw19@users.noreply.github.com>
2019-10-10 19:46:23 +0200

    closes #42 #43 #44 #45 #46 #47

--- bugueado_P115.java
+++ bugueado_P115.java
@@ -2,35 +2,29 @@ import java.util.*;
 
 public class bugueado_P115{
 	
-	public static void casoDePrueba(){
+	public static void main (String args[]){
+	Scanner in = new Scanner(System.in);	
 		
+	while (in.hasNext()){
 		long k=in.nextLong();
-
 		// Caso de salida
 		if (k == 0){
 			System.exit(0);
 		}
-
 		long cuadrado = k*k;
-		
 		String scuadrado = Long.toString(cuadrado);
 		boolean esKaprekar=false;
 		int f=0;
 		String izq="", der="";
-
 		if (k==1) esKaprekar=true;
-
 		while (f<=scuadrado.length() && !esKaprekar){
-		
 			// Recorremos el chars
-		
 			if (f==0){
 				izq=String.valueOf(scuadrado.charAt(f));
 			}	
 			else{
 				izq = scuadrado.substring(0,f);
-			}
-				
+			}	
 			der = scuadrado.substring(f,scuadrado.length()); 
 			if (der.equals("")){
 				der="0";
@@ -43,24 +37,15 @@ public class bugueado_P115{
 					esKaprekar=true;
 				}
 			}
-
 			f++;
-		
-		}
+		} //while
 		if(esKaprekar){
 			System.out.println("SI");
 		}else{
 			System.out.println("NO");
 		}
-
-	}
-
-	public static void main (String args[]){
-		Scanner in = new Scanner(System.in);
-		while (in.hasNext()){
-			casoDePrueba();
-		}
+		} //while hasnext()
 		System.exit(0);	
-	}
+	} //main
 
 }
