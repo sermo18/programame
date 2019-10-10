cc14dc4e6e728cdb0d87f15b235fcfb8e7068a0a
Sergio <55553719+sermo18@users.noreply.github.com>
2019-10-10 22:56:11 +0200

    Update p362.java

--- p362.java
+++ p362.java
@@ -1,24 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class p362 {
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+public class Fechasnavidad {
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+	 int n,i,dia,mes;
+	 i=0;
+	 n= sc.nextInt(); 
+	 
+	while(i <n) {
 		
-		int ncasos = sc.nextInt();
-		int dia;
-		int mes;
-
-		for(int i=0; i<ncasos; i++){
-			dia=sc.nextInt();
-			mes=sc.nextInt();
-			if(dia==25 && mes==12) {
-				System.out.println("SI");
-			} else {
-				System.out.println("NO");
-			}			
-			
+		dia= sc.nextInt();
+		mes = sc.nextInt();
+		
+		if(dia==25 && mes==12) {
+			System.out.println("SI");
+		} else {
+			System.out.println("NO");
 		}
-
+		i++;
+		
+		}
 	}
 }
